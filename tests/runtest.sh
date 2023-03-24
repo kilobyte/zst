e8585bef17797b89b2afca60e999903235d028f3
Adam Borowski <kilobyte@angband.pl>
2023-03-24 16:40:35 +0100

    Mark with T tests that don't need to be ran for every compressor.

--- tests/runtest.sh
+++ tests/runtest.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 
-if [ -z "$SRC" ] || [ -z "$BIN" ] || [ -z "$TOOL" ]; then
+if [ -z "$SRC" ] || [ -z "$BIN" ]; then
 	echo >&2 "Required ENV vars not set."
 	exit 1
 fi
@@ -15,5 +15,6 @@ export Z="$VG$BIN/zst" F="$SRC/zst.c"
 rm -rf "$TESTDIR"
 mkdir "$TESTDIR"
 cd "$TESTDIR"
-sh -e "$SRC/tests/$1.t"
+if [ -n "$TOOL" ]; then T=t; else T=T; fi
+sh -e "$SRC/tests/$1.$T"
 rm -rf "$TESTDIR"
